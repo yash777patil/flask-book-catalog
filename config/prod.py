9e489899ccb2361561c7f94cf290d02815f2c31c
yash777patil <yashpatil777yrp@gmail.com>
2022-01-22 20:49:56 +0530

    made corre

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,5 @@
+import os
 DEBUG =False
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:password@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
